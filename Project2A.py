2b1617d3c89bb22dbdd1dc76c46ac608421ea014
laffeyrin <142284601+laffeyrin@users.noreply.github.com>
2024-10-04 17:02:47 -0700

    Update Project2A.py

--- Project2A.py
+++ Project2A.py
@@ -1,7 +1,7 @@
 import random
 
 print("Welcome to R6 Randomizor")
-print("Are you playing on Defender or Attackers?")
+print("Are you playing on Defender or Attacker?")
 
 side = input("Enter 'Defender' or 'Attacker': ")
 
@@ -12,14 +12,14 @@ while side.capitalize() not in ['Defender', 'Attacker']:
 print(f"You are playing on {side.capitalize()}.")
 
 if side.capitalize() == 'Defender':
-    print("You will be playing as a defender. You will be given a random defender role.")
+    print("You will be playing as a defender. You will be generating random defenders.")
 else:
-    print("You will be playing as an attacker. You will be given a random attacker role.")
+    print("You will be playing as an attacker. You will be generating random attackers.")
 
 num_operators = int(input("Enter the number of operators you want to randomize: "))
 
-defender_roles = ["Ash", "Thermite", "Twitch", "Montagne", "Rook", "Doc", "Mute", "Smoke", "Castle", "Pulse", "Thatcher", "Frost", "Vigil"]
-attacker_roles = ["Fuze", "Glaz", "Buck", "Blackbeard", "Capitao", "Hibana", "Echo", "Ying", "Lesion", "Zofia", "Nomad", "Gridlock", "Nokk"]
+defender_roles = ["Alibi", "Aruni", "Azami", "Bandit", "Castle", "Caveira", "Clash", "Doc", "Echo", "Ela", "Fenrir", "Frost", "Goyo", "Jager", "Kaid", "Kapkan", "Lesion", "Maestro", "Melusi", "Mira", "Mozzie", "Mute", "Oryx", "Pulse", "Sentry", "Rook", "Skopos", "Smoke", "Solis", "Tachanka", "Thorn", "Thunderbird", "Tubarao", "Valkyrie", "Vigil", "Wamai", "Warden"]
+attacker_roles = ["Ace", "Amaru", "Ash", "Blackbeard", "Blitz", "Brava", "Buck", "Capitao", "Deimos", "Dokkaebi", "Finka", "Flores", "Fuze", "Glaz", "Gridlock", "Grim", "Hibana", "Iana", "IQ", "Jackal", "Kali", "Lion", "Maverick", "Montagne", "Nomad", "Nokk", "Osa", "Ram", "Sens", "Sledge", "Striker", "Thatcher", "Thermite", "Twitch", "Ying", "Zero", "Zofia"]
 
 def randomize_operators(num_operators, roles):
     if num_operators > len(roles):
@@ -30,11 +30,8 @@ def randomize_operators(num_operators, roles):
 
 if side.capitalize() == 'Defender':
     defender_operators = randomize_operators(num_operators, defender_roles)
-    attacker_operators = randomize_operators(num_operators, attacker_roles)
+    print(f"Your defender operators are: {', '.join(defender_operators)}")
+
 else:
     attacker_operators = randomize_operators(num_operators, attacker_roles)
-    defender_operators = randomize_operators(num_operators, defender_roles)
-
-if defender_operators and attacker_operators:
-    print(f"Your random defender operators are: {', '.join(defender_operators)}")
     print(f"Your random attacker operators are: {', '.join(attacker_operators)}")
